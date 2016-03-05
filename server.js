f3bebd312540787a0d46a4a6eec965153fbcc8f0
bhou14 <binghou@BING-HOUs-iMac-3.local>
2016-03-05 01:34:49 -0500

    add openshift env variables

--- server.js
+++ server.js
@@ -74,5 +74,8 @@ fs.readdir(path.normalize(__dirname + '/public/audio'), function(err, files){
 });
 
 // Start the server
-app.listen(3000);
-console.log("bettershow app listening on port 3000");
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
+});
